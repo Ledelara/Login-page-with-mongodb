b9685e3b3e2c8f29c33a561d7e59df23b7c0383d
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-25 18:54:37 -0300

    ✨ Feat: RegisterForm

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,42 +2,35 @@
 import { useState } from "react";
 import useCreateUser from "../../services/mutate";
 import { IUser } from "../../Types/types";
-
+import RegisterForm from "@/components/Forms/RegisterForm";
 
 export default function HomePage() {
-    const { mutate } = useCreateUser();
+    const { createUserMutation, loading } = useCreateUser();
     const [user, setUser] = useState<IUser>({
         name: '',
         email: '',
         password: ''
-    })
+    });
 
-    const handleSubmit = (e:React.FormEvent) => {
+    const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        mutate(user);
-    }
+        createUserMutation.mutate(user);
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setUser(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Name"
-                value={user.name}
-                onChange={(e) => setUser({ ...user, name: e.target.value })}
-            />
-            <input
-                type="email"
-                placeholder="Email"
-                value={user.email}
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={user.password}
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-            />
-            <button type="submit">Salvar</button>
-        </form>
+        <RegisterForm
+            values={user}
+            onSubmit={handleSubmit}
+            onChange={handleChange}
+            disabled={loading}
+        />
     )
 }
