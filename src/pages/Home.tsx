ba6f6774f58f7a278ef3f72b4cfa0d449cb83985
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-28 17:21:44 -0300

    ♻️ Refactor: envolvendo Home.tsx nos contextos da aplicação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
 'use client'
+import { AlertPopUp } from "@/components/Alert/Alert";
 import RegisterForm from "@/components/Forms/RegisterForm";
+import { AlertProvider } from "@/context/AlertContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function HomePage() {
+    const queryClient = new QueryClient();
     
     return (
-        <RegisterForm />
+        <QueryClientProvider client={queryClient}>
+            <AlertProvider>
+                <RegisterForm />
+                <AlertPopUp />
+            </AlertProvider>
+        </QueryClientProvider>
     )
 }
