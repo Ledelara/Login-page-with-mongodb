15ff536e9b98ccd50fd96443db61e44530b32edb
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-29 00:48:08 -0300

    ♻️ Refactor: removendo useRouter

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import { TextField, Button, Container } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -8,18 +7,14 @@ import { useLogin } from "../../../services/mutate";
 
 const LoginForm = () => {
   const { loginMutation, loading, error } = useLogin();
-  const router = useRouter();
 
   const { register, handleSubmit, formState: { errors } } = useForm<ILogin>({
     resolver: zodResolver(loginSchema),
   });
 
   const onSubmit = (data: ILogin) => {
-    loginMutation.mutate({ email: data.email, password: data.password }, {
-      onSuccess: () => {
-        router.push("/");
-      }
-    });
+    loginMutation.mutate({ email: data.email, password: data.password }
+    );
   };
 
   return (
