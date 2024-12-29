dab506dfe862b1bd13f13c956d9715af2f07d2d1
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-29 14:54:30 -0300

    ✨ Feat: useLoginUser

--- src/services/mutate.ts
+++ src/services/mutate.ts
@@ -1,6 +1,6 @@
 'use client'
 import { useMutation } from "@tanstack/react-query"
-import { createUser } from "./api";
+import { createUser, loginUser } from "./api";
 import { useState } from "react";
 import { useAlert } from "@/hooks/useAlert";
 
@@ -37,4 +37,37 @@ const useCreateUser = () => {
     };
 }
 
-export default useCreateUser;
+const useLoginUser = () => {
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const { showAlert } = useAlert()
+
+    const loginUserMutation = useMutation({
+        mutationFn: loginUser,
+        onMutate: () => {
+            setLoading(true);
+            setError(null);
+        },
+        onSuccess: (data) => {
+            console.log("User logged in:", data);
+            setLoading(false);
+            showAlert("Usuário logado com sucesso!", "success");
+        },
+        onError: (error: Error) => {
+            console.error("Error logging in:", error);
+            setError("Ocorreu um erro ao fazer login. Tente novamente.");
+            setLoading(false);
+        },
+        onSettled: () => {
+            setLoading(false);
+        },
+    });
+
+    return {
+        loginUserMutation,
+        loading,
+        error,
+    };
+}
+
+export { useCreateUser, useLoginUser };
