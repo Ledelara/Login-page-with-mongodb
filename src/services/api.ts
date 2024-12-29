73ac1e9fdda343ecc42e43735f46fd888e161a31
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-29 14:27:51 -0300

    ♻️ Refactor: fazendo requisição POST ao servidor

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,12 +2,17 @@ import axios from "axios";
 import { IUser } from "../@types/types";
 
 const api = axios.create({
-    baseURL: "/api",
+    baseURL: "https://auth-server-mongodb.onrender.com/api",
 });
 
 export const createUser = async (user: IUser): Promise<IUser> => {
-    const response = await api.post<IUser>("/users", user);
+    const response = await api.post<IUser>("/auth/register", user);
     return response.data;
 };
+
+export const loginUser = async (user: IUser): Promise<IUser> => {
+    const response = await api.post<IUser>("/auth/login", user);
+    return response.data;   
+}
 
 export default api;
