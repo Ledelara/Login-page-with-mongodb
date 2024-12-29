3dbb3fb85507f514844a86b2cb071e33fe67b4c6
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-28 23:16:35 -0300

    ♻️ Refactor: envolvendo o componente Register nos providers necessários

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,8 +1,17 @@
 'use client'
+import { AlertPopUp } from "@/components/Alert/Alert";
 import RegisterForm from "@/components/Forms/RegisterForm";
+import { AlertProvider } from "@/context/AlertContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Register() {
+    const queryClient = new QueryClient();
     return (
-        <RegisterForm />
+        <QueryClientProvider client={queryClient}>
+            <AlertProvider>
+                <RegisterForm />
+                <AlertPopUp />
+            </AlertProvider>
+        </QueryClientProvider>
     );
 }
