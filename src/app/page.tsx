5341ed28330f051557d26809109c1d2ee6c6c6f5
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-29 00:30:58 -0300

    🐛 Fix: envolvendo Router com AuthProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import HomePage from "@/pages/home";
+import Router from "@/routes/Router";
 import styles from "./page.module.css";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <HomePage />
-      </main>
-    </div>
+    <AuthProvider>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <Router />
+        </main>
+      </div>
+    </AuthProvider>
   );
 }
