cfd2b553aa4053156fbffe14c8edae7f6f9f50c4
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-30 10:48:31 -0300

    ♻️ Refactor: refatorando Router

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,42 +1,39 @@
 'use client';
-import { AuthProvider, useAuth } from "@/context/AuthContext.";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ROUTE_ENDPOINTS } from "@/services/constants/RouteConstants";
-import { ReactNode } from "react";
 import LoginPage from "@/pages/login";
 import RegisterPage from "@/pages/register";
 import HomePage from "@/pages/Home";
+import { useAuth } from "@/context/AuthContext.";
 
 export default function Router() {
     const { isAuthenticated } = useAuth();
 
-    function publicRoute(path: string, element: ReactNode) {
+    function publicRoute(path: string, element: React.ReactNode) {
         return (
             <Route
                 path={path}
-                element={isAuthenticated ? <Navigate to={ROUTE_ENDPOINTS.HOME} /> : element}
+                element={isAuthenticated ? <Navigate to={ROUTE_ENDPOINTS.HOME} replace /> : element}
             />
-        )
-    };
+        );
+    }
 
-    function AuthRoute(path: string, element: ReactNode) {
+    function AuthRoute(path: string, element: React.ReactNode) {
         return (
             <Route
                 path={path}
-                element={isAuthenticated ? element : <Navigate to={ROUTE_ENDPOINTS.LOGIN} />}
+                element={isAuthenticated ? element : <Navigate to={ROUTE_ENDPOINTS.LOGIN} replace />}
             />
-        )
-    };
+        );
+    }
 
     return (
-        <AuthProvider>
-            <BrowserRouter>
-                <Routes>
-                    {publicRoute(ROUTE_ENDPOINTS.LOGIN, <LoginPage />)}
-                    {publicRoute(ROUTE_ENDPOINTS.REGISTER, <RegisterPage />)}
-                    {AuthRoute(ROUTE_ENDPOINTS.HOME, <HomePage />)}
-                </Routes>
-            </BrowserRouter>
-        </AuthProvider>
-    )
+        <BrowserRouter>
+            <Routes>
+                {publicRoute(ROUTE_ENDPOINTS.LOGIN, <LoginPage />)}
+                {publicRoute(ROUTE_ENDPOINTS.REGISTER, <RegisterPage />)}
+                {AuthRoute(ROUTE_ENDPOINTS.HOME, <HomePage />)}
+            </Routes>
+        </BrowserRouter>
+    );
 }
